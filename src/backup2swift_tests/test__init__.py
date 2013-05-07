f2622fe23bfd41511b7da5a60ae174581d7bc33c
Kouhei Maeda <mkouhei@palmtb.net>
2013-05-07 19:03:31 +0900

    add skelton of unit test code
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ src/backup2swift_tests/test__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2013 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2013 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import unittest
+import sys
+import os.path
+sys.path.append(os.path.abspath('src'))
+import backup2swift.__init__ as i
+
+
+class InitTests(unittest.TestCase):
+
+    def test__version__(self):
+        self.assertTrue(i.__version__)
+
+    def test__name__(self):
+        self.assertTrue(i.__NAME__)
---
+++ src/backup2swift_tests/test_command.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2013 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import unittest
+import sys
+import os.path
+sys.path.append(os.path.abspath('src'))
+import backup2swift.command as c
+
+
+class CommandTests(unittest.TestCase):
+
+    def test_parse_options(self):
+        pass
+
+    def test_setoption(self):
+        pass
