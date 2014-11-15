d25b830e9f5f2b0de7b1e1bd9c32d66a291a1365
Kouhei Maeda <mkouhei@palmtb.net>
2014-11-16 07:36:46 +0900

    Appended pass through tox from python setup.py test.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -20,12 +20,17 @@ import os.path
 import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-import multiprocessing
 
 import backup2swift
 
 
 class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', 'Arguments to pass to tox')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
@@ -33,7 +38,11 @@ class Tox(TestCommand):
 
     def run_tests(self):
         import tox
-        errno = tox.cmdline(self.test_args)
+        import shlex
+        if self.tox_args:
+            errno = tox.cmdline(args=shlex.split(self.tox_args))
+        else:
+            errno = tox.cmdline(self.test_args)
         sys.exit(errno)
 
 
